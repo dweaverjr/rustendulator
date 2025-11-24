9aeb2e6e019cd1ecd5190a2ba7c985d19f0a9042
dweaverjr <80757650+dweaverjr@users.noreply.github.com>
2025-11-23 21:47:10 -0500

    Stubs of some CPU instructions and update opcode table with new entries

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -4,4 +4,32 @@ impl Cpu {
     pub(super) fn brk(&mut self) {
         todo!("BRK not implemented yet")
     }
+
+    pub(super) fn ora(&mut self) {
+        todo!("ORA not implemented yet")
+    }
+
+    pub(super) fn kil(&mut self) {
+        todo!("KIL not implemented yet")
+    }
+
+    pub(super) fn slo(&mut self) {
+        todo!("SLO not implemented yet")
+    }
+
+    pub(super) fn nop(&mut self) {
+        todo!("NOP not implemented yet")
+    }
+
+    pub(super) fn asl(&mut self) {
+        todo!("ASL not implemented yet")
+    }
+
+    pub(super) fn php(&mut self) {
+        todo!("PHP not implemented yet")
+    }
+
+    pub(super) fn anc(&mut self) {
+        todo!("ANC not implemented yet")
+    }
 }
--- src/cpu/opcodes.rs
+++ src/cpu/opcodes.rs
@@ -16,10 +16,10 @@ pub(super) enum AddressingMode {
 }
 
 // Records for the opcode table
-struct OpcodeRecord {
+pub(super) struct OpcodeRecord {
     mnemonic: &'static str,
-    instruction_call: fn(&mut Cpu),
-    base_cycles: u8,
+    function: fn(&mut Cpu),
+    cycles: u8,
     addressing_mode: AddressingMode,
     page_cross_penalty: bool,
 }
@@ -27,21 +27,34 @@ struct OpcodeRecord {
 // Helper function to represent record more cleanly
 const fn opcode(
     mnemonic: &'static str,
-    instruction_call: fn(&mut Cpu),
-    base_cycles: u8,
+    function: fn(&mut Cpu),
+    cycles: u8,
     addressing_mode: AddressingMode,
     page_cross_penalty: bool,
 ) -> OpcodeRecord {
     OpcodeRecord {
         mnemonic,
-        instruction_call,
-        base_cycles,
+        function,
+        cycles,
         addressing_mode,
         page_cross_penalty,
     }
 }
 
-pub(super) const OPCODE_TABLE: [OpcodeRecord; 1] =
+pub(super) const OPCODE_TABLE: [OpcodeRecord; 14] =
     [
         /* 0x00 */ opcode("BRK", Cpu::brk, 7, AddressingMode::Implicit, false),
+        /* 0x01 */ opcode("ORA", Cpu::ora, 6, AddressingMode::IndirectX, false),
+        /* 0x02 */ opcode("KIL", Cpu::kil, 0, AddressingMode::Implicit, false),
+        /* 0x03 */ opcode("SLO", Cpu::slo, 8, AddressingMode::IndirectX, false),
+        /* 0x04 */ opcode("NOP", Cpu::nop, 3, AddressingMode::ZeroPage, false),
+        /* 0x05 */ opcode("ORA", Cpu::ora, 3, AddressingMode::ZeroPage, false),
+        /* 0x06 */ opcode("ASL", Cpu::asl, 5, AddressingMode::ZeroPage, false),
+        /* 0x07 */ opcode("SLO", Cpu::slo, 5, AddressingMode::ZeroPage, false),
+        /* 0x08 */ opcode("PHP", Cpu::php, 3, AddressingMode::Implicit, false),
+        /* 0x09 */ opcode("ORA", Cpu::ora, 2, AddressingMode::Immediate, false),
+        /* 0x0A */ opcode("ASL", Cpu::asl, 2, AddressingMode::Accumulator, false),
+        /* 0x0B */ opcode("ANC", Cpu::anc, 2, AddressingMode::Immediate, false),
+        /* 0x0C */ opcode("NOP", Cpu::nop, 4, AddressingMode::Absolute, false),
+        /* 0x0D */ opcode("ORA", Cpu::ora, 4, AddressingMode::Absolute, false),
     ];
