0cd5b0303f64fa4cadc425a9d67ed9a63acb4e8f
dweaverjr <80757650+dweaverjr@users.noreply.github.com>
2025-11-25 20:55:38 -0500

    More opcodes and instruction stubs

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -64,4 +64,80 @@ impl Cpu {
     pub(super) fn plp(&mut self) {
         todo!("PLP not implemented yet")
     }
+
+    pub(super) fn bmi(&mut self) {
+        todo!("BMI not implemented yet")
+    }
+
+    pub(super) fn sec(&mut self) {
+        todo!("SEC not implemented yet")
+    }
+
+    pub(super) fn rti(&mut self) {
+        todo!("RTI not implemented yet")
+    }
+
+    pub(super) fn eor(&mut self) {
+        todo!("EOR not implemented yet")
+    }
+
+    pub(super) fn sre(&mut self) {
+        todo!("SRE not implemented yet")
+    }
+
+    pub(super) fn lsr(&mut self) {
+        todo!("LSR not implemented yet")
+    }
+
+    pub(super) fn pha(&mut self) {
+        todo!("PHA not implemented yet")
+    }
+
+    pub(super) fn alr(&mut self) {
+        todo!("ALR not implemented yet")
+    }
+
+    pub(super) fn jmp(&mut self) {
+        todo!("JMP not implemented yet")
+    }
+
+    pub(super) fn bvc(&mut self) {
+        todo!("BVC not implemented yet")
+    }
+
+    pub(super) fn cli(&mut self) {
+        todo!("CLI not implemented yet")
+    }
+
+    pub(super) fn rts(&mut self) {
+        todo!("RTS not implemented yet")
+    }
+
+    pub(super) fn adc(&mut self) {
+        todo!("ADC not implemented yet")
+    }
+
+    pub(super) fn rra(&mut self) {
+        todo!("RRA not implemented yet")
+    }
+
+    pub(super) fn ror(&mut self) {
+        todo!("ROR not implemented yet")
+    }
+
+    pub(super) fn pla(&mut self) {
+        todo!("PLA not implemented yet")
+    }
+
+    pub(super) fn arr(&mut self) {
+        todo!("ARR not implemented yet")
+    }
+
+    pub(super) fn bvs(&mut self) {
+        todo!("BVS not implemented yet")
+    }
+
+    pub(super) fn sei(&mut self) {
+        todo!("SEI not implemented yet")
+    }
 }
