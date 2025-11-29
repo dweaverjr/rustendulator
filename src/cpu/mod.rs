e1dd0caee405ef0ec4468263865f027f878c3856
dweaverjr <80757650+dweaverjr@users.noreply.github.com>
2025-11-28 21:31:21 -0500

    Add addressing mode and page mode penalty

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,13 +2,16 @@ mod instructions;
 mod opcodes;
 mod registers;
 use self::registers::CpuRegisters;
+use self::opcodes::AddressingMode;
 use crate::bus::Bus;
 
 pub(crate) struct Cpu {
     registers: CpuRegisters,
     bus: *mut Bus,
     cycle_counter: u8,
-    current_handler: Option<fn(&mut Cpu)>
+    current_handler: Option<fn(&mut Cpu)>,
+    current_addressing_mode: AddressingMode,
+    current_page_mode_penalty: bool,
 }
 
 impl Cpu {
@@ -17,16 +20,19 @@ impl Cpu {
             registers: CpuRegisters::new(),
             bus: bus,
             cycle_counter: 0,
-            current_handler: None
+            current_handler: None,
+            current_addressing_mode: AddressingMode::Implicit,
+            current_page_mode_penalty: false,
         }
     }
 
     pub fn tick(&mut self) {
         // Approach is, exhaust cycles until the last, then execute. Mid instruction quirks are easier to deal with
         if self.cycle_counter > 0 {
-            // todo: track interrupts for BRK etc.
+            // TODO: track interrupts for BRK etc.
             self.cycle_counter -= 1;
             if self.cycle_counter == 0 {
+                // Even if the instruction incurs cycle penalties to then burn down, the instruction will still only execute once via Option
                 if let Some(handler) = self.current_handler.take() {
                     handler(self); // Execute the intruction handler
                 }
@@ -34,14 +40,16 @@ impl Cpu {
             return; 
         }
 
-        // todo: DMA handling later
+        // TODO: DMA handling later
 
         let opcode = self.fetch_byte();
         let opcode_record = &opcodes::OPCODE_TABLE[opcode as usize];
-        // Burn one cycle for the fetch
+        // Burn one cycle for the fetch and decode
         self.cycle_counter = opcode_record.cycles - 1;
         // Store the handler for later when cycle counter is exhausted
         self.current_handler = Some(opcode_record.handler);
+        self.current_addressing_mode = opcode_record.addressing_mode;
+        self.current_page_mode_penalty = opcode_record.page_cross_penalty;
 
     }
 
