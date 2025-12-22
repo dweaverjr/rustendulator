f45a4dd4751560f908093f45d63a5b2d7d58e7d5
dweaverjr <80757650+dweaverjr@users.noreply.github.com>
2025-12-22 14:03:31 -0500

    Empty skeleton of gui

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,5 +1,24 @@
 use eframe::egui;
+use rustendulator_core::Nes;
 
-fn main() {
-    
+fn main() -> Result<(), eframe::Error> {
+    eframe::run_native(
+        "Rustendulator",
+        eframe::NativeOptions::default(),
+        Box::new(|_| Ok(Box::new(Rustendulator::default()))),
+    )
+}
+
+struct Rustendulator {
+    nes: Nes,
+}
+
+impl Default for Rustendulator {
+    fn default() -> Self {
+        Self { nes: Nes::new() }
+    }
+}
+
+impl eframe::App for Rustendulator {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {}
 }
